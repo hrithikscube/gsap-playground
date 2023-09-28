8d377f16dd63c943477314253f085fa7c7e5da17
hrithikscube <hrithik@scube.me>
2023-09-28 11:02:01 +0530

    clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
-import Chat from './Chat'
-import Temp from './Temp'
 import ScaleAnimation from './components/pages/ScaleAnimation'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<ScaleAnimation />} />
-      <Route path="/temp" element={<Temp />} />
-      <Route path="/demo" element={<ScaleAnimation />} />
     </Routes>
   )
 }
